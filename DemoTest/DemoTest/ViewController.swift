49bc875881b21948f310e3f4bf932afb1b7a0b6a
mohammad <csit.yunus23@gmail.com>
2018-11-22 14:24:14 +0530

    Project Setup
    Project created and added.

---
+++ DemoTest/DemoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoTest
+//
+//  Created by Mohammad Yunus on 22/11/18.
+//  Copyright © 2018 Mohammad Yunus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
