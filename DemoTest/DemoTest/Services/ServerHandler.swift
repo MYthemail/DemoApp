ecf8bca37669ac6573d49126aae016b4883b52be
mohammad <csit.yunus23@gmail.com>
2018-11-22 16:03:08 +0530

    Network and Model classes added
    VModel, ViewModel and Network classes added.

---
+++ DemoTest/DemoTest/Services/ServerHandler.swift
@@ -0,0 +1,110 @@
+//
+//  ServerHandler.swift
+//  DemoTest
+//
+//  Created by Mohammad Yunus on 22/11/18.
+//  Copyright © 2018 Mohammad Yunus. All rights reserved.
+//
+
+
+import UIKit
+import SystemConfiguration
+
+/*
+- Network Activity
+- Fetch server data
+
+*/
+
+class ServerHandler: NSObject {
+	
+	/// Network Activity for checking the network in device
+	class func isNetWorkAvailable() -> Bool {
+		var zeroAddress = sockaddr_in()
+		zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+		zeroAddress.sin_family = sa_family_t(AF_INET)
+		
+		guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+			$0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+				SCNetworkReachabilityCreateWithAddress(nil, $0)
+			}
+		}) else {
+			return false
+		}
+		
+		var flags: SCNetworkReachabilityFlags = []
+		if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+			return false
+		}
+		
+		let isReachable = flags.contains(.reachable)
+		let needsConnection = flags.contains(.connectionRequired)
+		return (isReachable && !needsConnection)
+		
+	}
+	
+	class func sendGetRequest(functionName : String, showLoader: Bool,  completionHandler:@escaping ((_ responseValue: Any?, _ error: Error?) -> Void)) -> Void {
+		/// Check Network Availability
+		
+		if isNetWorkAvailable() == true {
+			// Show Activity
+			if showLoader {
+				DispatchQueue.main.async(execute: {
+					ActivityIndicatorView.showActivity()
+				})
+			}
+			
+			/// Set up the URL request
+			guard let url = URL(string: functionName) else {
+				print("Error: cannot create URL")
+				return
+			}
+			let urlRequest = URLRequest(url: url)
+			
+			/// set up the session
+			let config = URLSessionConfiguration.default
+			let session = URLSession(configuration: config)
+			
+			/// make the request
+			let task = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+				
+				/// Hide Activity
+				if showLoader {
+					DispatchQueue.main.async(execute: {
+						ActivityIndicatorView.hideActivity()
+					})
+				}
+				
+				/*
+				Check the server data and error if we get error result equal to nil
+				then we immediate return the function
+				*/
+				guard let data = data, error == nil else {
+					return
+				}
+				let httpStatus = response as? HTTPURLResponse
+				
+				/*
+				Check server response status code
+				
+				- Success: If we get status code equal to 200 it means we get success response
+				and return success result to the function.
+				- Failure: If we get status code not equal to 200 then return the error message  to the function
+				
+				*/
+				if httpStatus?.statusCode == 200 {
+					/// Convert first data to UTF8 encoding
+					let utfStringData = String(data: data, encoding: String.Encoding.ascii)
+					completionHandler(utfStringData, nil)
+				} else {
+					completionHandler(nil, error)
+				}
+			})
+			task.resume()
+			
+		} else {
+			AlertView.showAlert(title: Messages.Network.title, message: Messages.Network.message, cancelBtnTitle: "OK")
+		}
+	}
+	
+}
---
+++ DemoTest/DemoTest/ViewModels/AboutNewsFeedViewModel.swift
@@ -0,0 +1,99 @@
+//
+//  AboutNewsFeedViewModel.swift
+//  DemoTest
+//
+//  Created by Mohammad Yunus on 22/11/18.
+//  Copyright © 2018 Mohammad Yunus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SDWebImage
+
+class AboutNewsFeedViewModel {
+	var aboutNewsDatas = [NewsFeed]()
+	
+	// MARK:- Get About NewsFeed Data From Server
+	
+	/// Get the NewsFeed data from server using REST API.
+	///
+	/// - Parameter completionBlock: completion block to notify get the response from REST API
+	
+	func getAboutNewsFeedData(completionBlock : @escaping (() ->())) {
+		let aboutNewsFeedUrl = APIPaths.baseUrl
+		
+		ServerHandler.sendGetRequest(functionName: aboutNewsFeedUrl, showLoader: true) { (result, error) in
+			//Success result from the server
+			if error == nil {
+				/// Remove old data from array
+				self.aboutNewsDatas.removeAll()
+				
+				let response = self.convertToDictionary(text: result as! String)
+				if let responseData = response!["results"], responseData is [[String: Any]]{
+					let responseDictionary = responseData as! [[String : Any]]
+					for newsFeed in responseDictionary {
+						if let news = NewsFeed(data: newsFeed) {
+							self.aboutNewsDatas.append(news)
+						}
+					}
+				}
+			} else {
+				/// In case if server send the error then show the message
+				AlertView.showAlert(title: "Alert!", message: "Can't load items", cancelBtnTitle: "OK")
+			}
+			completionBlock()
+			
+		}
+	}
+	
+	// MARK: Convert String to Dictionary
+	
+	/// Convert String to Dictionary with jsonObject method of JSONSerialization
+	///
+	/// - Parameter text: pass the valid string
+	/// - Returns: Optional Dictionary of type [String: Any]?
+	func convertToDictionary(text: String) -> [String: Any]? {
+		if let data = text.data(using: .utf8) {
+			do {
+				return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+			} catch {
+				print(error.localizedDescription)
+			}
+		}
+		return nil
+	}
+	
+	//MARK: Return number of rows
+	
+	/// Get the count of the News to show news list
+	///
+	/// - Returns: total number of news count
+	
+	func numberofRows() -> Int {
+		return aboutNewsDatas.count
+	}
+	
+	//MARK: Return About News data for showing on cell
+	
+	/// Get the news corresponding to passed index path
+	///
+	/// - Parameter indexPath: pass the valid/selected index path
+	/// - Returns: AboutNews object
+	
+	func getNewsFeedRecord(indexPath : IndexPath) -> NewsFeed {
+		return aboutNewsDatas[indexPath.row]
+	}
+	
+	
+}
+extension UIImageView {
+	
+	func setImage(_ url: String) {
+		guard url.isEmpty == false else {
+			return
+		}
+		if let uRL = URL(string: url) {
+			self.sd_setImage(with: uRL, completed: nil)
+		}
+	}
+}
