3a883c2abffa530f916fde5b4d7b936b591f2050
mohammad <csit.yunus23@gmail.com>
2018-11-22 17:29:47 +0530

    Test cases added.
    Success and Fail test cases added.

--- DemoTest/DemoTestTests/DemoTestTests.swift
+++ DemoTest/DemoTestTests/DemoTestTests.swift
@@ -10,25 +10,106 @@ import XCTest
 @testable import DemoTest
 
 class DemoTestTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+	
+	var newsFeedFailableSUT:NewsFeed!
+	var newsFeedSUT:NewsFeed!
+	
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+		
+		self.newsFeedFailableSUT = NewsFeed(data: self.loadDummyNewsFeedFailableData())
+		self.newsFeedSUT = NewsFeed(data: self.loadDummyNewsFeedData())
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+		
+		self.newsFeedSUT = nil
+		self.newsFeedFailableSUT = nil
+	}
+	
+	func testExample() {
+		// This is an example of a functional test case.
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	}
+	
+	func testPerformanceExample() {
+		// This is an example of a performance test case.
+		self.measure {
+			// Put the code you want to measure the time of here.
+		}
+	}
+	//MARK: Synchronus Test
+	func testNewsFeedFailableInitializer() {
+		
+		XCTAssertNotNil(self.newsFeedSUT, "Failed to create object.")
+		
+		//XCTAssertNotNil(self.newsFeedFailableSUT, "Failed to create object.")
+	}
+	
+	
+	
+	//MARKL: Asynchronous Test
+	func testNewsFeedListing() {
+		
+		/// Get expectation object
+		let expect = expectation(description: "Success")
+		
+		let aboutNewsFeedUrl = APIPaths.baseUrl
+		
+		ServerHandler.sendGetRequest(functionName: aboutNewsFeedUrl, showLoader: true) { (result, error) in
+			//Success result from the server
+			if error == nil {
+				
+				let response = self.convertToDictionary(text: result as! String)
+				
+				let status = response!["status"] as? String ?? ""
+				if status == "OK" {
+					XCTAssertEqual(status, "OK")
+					expect.fulfill()
+				}else {
+					XCTFail()
+				}
+				
+			} else {
+				print(error ?? "")
+				XCTAssertNil(error, "Unexpected error occured: \(String(describing: error?.localizedDescription))")
+			}
+		}
+		///We have to update timeout value corresponding to request time out
+		waitForExpectations(timeout: 50) { (error) in
+			XCTAssertNil(error, "Test timed out. \(error!.localizedDescription)")
+		}
+	}
+	
+	//MARK: Helper Static Data
+	func loadDummyNewsFeedFailableData() -> [String : Any] {
+		return ["title" : "",
+				"byline" : "By DIONNE SEARCEY and EMMANUEL AKINWOTU",
+				"published_date" : "2018-11-02"
+		]
+	}
+	
+	func loadDummyNewsFeedData() -> [String : Any] {
+		return ["title" : "The New York Times",
+				"byline" : "By CAMPBELL ROBERTSON, CHRISTOPHER MELE and SABRINA TAVERNISE",
+				"published_date" : "2018-10-27"
+		]
+	}
+	
+	
+	
+	func convertToDictionary(text: String) -> [String: Any]? {
+		if let data = text.data(using: .utf8) {
+			do {
+				return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+			} catch {
+				print(error.localizedDescription)
+			}
+		}
+		return nil
+	}
+	
 }
