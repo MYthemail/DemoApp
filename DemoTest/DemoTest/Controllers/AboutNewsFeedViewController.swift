d8759b7757fc0c4987906caf65ac1c854c2daeee
mohammad <csit.yunus23@gmail.com>
2018-11-22 14:43:36 +0530

    Structure the project
    structure the projust and "AboutNewsFeedViewController" class added.

---
+++ DemoTest/DemoTest/Controllers/AboutNewsFeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutNewsFeedViewController.swift
+//  DemoTest
+//
+//  Created by Mohammad Yunus on 22/11/18.
+//  Copyright © 2018 Mohammad Yunus. All rights reserved.
+//
+
+import UIKit
+
+class AboutNewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
